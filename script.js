a8d225ea051fb2dddb7bc65180502e272c99e85c
FixieGemini <109640240+FixieGemini@users.noreply.github.com>
2022-08-28 07:18:18 -0700

    created and styled timeblock

--- script.js
+++ script.js
@@ -1,4 +1,30 @@
-var $currentDay = $("#currentDay");
-var currentDate = moment().format("dddd, MMMM Do");
+var currentDay = $("#currentDay");
+var timeBlocks = $(".time-block");
 
-$currentDay.text(currentDate);
+var currentDate = moment().format("dddd, MMMM Do YYYY");
+var currentHour = moment().format("H");
+
+var toDoItems = [];
+
+//Show current date
+currentDay.text(currentDate);
+
+//Format timeblock colors based on current time
+function colorTimeBlocks(){
+    timeBlocks.each(function(){
+      var thisBlock = $(this);
+      var thisBlockHr = parseInt(thisBlock.attr("data-hour"));
+
+      if (thisBlockHr === currentHour) {
+        thisBlock.addClass("present").removeClass("past future");
+      }
+      if (thisBlockHr < currentHour) {
+        thisBlock.addClass("past").removeClass("present future");
+      }
+      if (thisBlockHr > currentHour) {
+        thisBlock.addClass("future").removeClass("past present");
+      }
+    });
+}
+
+colorTimeBlocks();
